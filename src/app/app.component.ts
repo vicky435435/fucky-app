be14c12469a445de906121e10b9e42c247deeed5
Ramkrushna435 <ramkrushna.m@smartlybuilt.com>
2023-10-31 14:08:23 +0530

    code update side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+// import { register } from 'swiper/element/bundle';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { Router } from '@angular/router';
 // import { NavController, Content, Slides } from 
-
+// import { IonicSlides } from '@ionic/angular';
+import { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, AfterContentChecked{
   // @ViewChild(Slides) slides: Slides;
  
   // public images: any = [{
@@ -18,33 +19,45 @@ export class HomePage implements OnInit {
 
   // }];
 
-  slideOpts = {
-    speed: 400,
-     // initialSlide: 0,
-     // slidesPerView:1,
-    autoplay: true,
-    loop: true,
-    // autoplay: {
-    //   delay: 4000
-    // }
-  };
+  // swiperModules = [IonicSlides];
 
+  // slideOpts = {
+  //   speed: 400,
+  //    // initialSlide: 0,
+  //    // slidesPerView:1,
+  //   autoplay: true,
+  //   loop: true,
+  //   // autoplay: {
+  //   //   delay: 4000
+  //   // }
+  // };
+
+ 
   constructor(
     private menuController: MenuController,
     private router: Router
   ) {}
 
+  bannerConfig! : SwiperOptions;
+  banners: any[] = [];
+
 
   ngOnInit() {
+    this.banners = [
+      {banner: '../../../../assets/images/IMG-20231030-WA0000.jpg'},
+      {banner: '../../../../assets/images/IMG-20231030-WA0000.jpg'},
+      {banner: '../../../../assets/images/IMG-20231030-WA0000.jpg'},
+      {banner: '../../../../assets/images/IMG-20231030-WA0000.jpg'},
+    ];
   }
 
-  closeMenu() {
-    this.menuController.close();
-  }
-
-  openPage(page: string) {
-    this.router.navigate([page]);
-    this.menuController.close();
+ 
+  ngAfterContentChecked(): void {
+    this.bannerConfig = {
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+      centeredSlides: true
+    };
   }
 
   
